f30f92748b9b9c57feb05f4a6482fa5df6635619
KiranNarayananE <kirannarayanan999@gmail.com>
2023-10-31 19:42:27 +0530

    link change

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios"
 import { Store } from "../Redux/Store";
 const axiosInstance = Axios.create({
-    baseURL: "http://localhost:4000/",
+    baseURL: "https://pdf-extractor-server-0z09.onrender.com/",
     headers: {
       "Content-Type": "application/json",
     },
